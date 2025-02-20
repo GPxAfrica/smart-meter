36f43a390879c3ddf05d9fc3403b0db12e3cf321
gerrit <gerritpch@gmail.com>
2025-02-20 16:57:50 +0100

    comments in PasswordMatchesValidator

--- src/main/java/com/example/smartmetergateway/validation/PasswordMatchesValidator.java
+++ src/main/java/com/example/smartmetergateway/validation/PasswordMatchesValidator.java
@@ -4,14 +4,19 @@ import com.example.smartmetergateway.model.UserDto;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class PasswordMatchesValidator
-  implements ConstraintValidator<PasswordMatches, Object> {
-    
+/*
+Klasse wird verwendet, um zu überprüfen, ob das Passwort und die Bestätigung des Passworts übereinstimmen.
+Dient als Validator für die Annotation @PasswordMatches.
+Als Teil der Bean Validation integriert, wodurch die Validierung automatisch aufgerufen wird, wenn die Annotation @PasswordMatches verwendet wird.
+ */
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
     @Override
     public void initialize(PasswordMatches constraintAnnotation) {
     }
+
     @Override
-    public boolean isValid(Object obj, ConstraintValidatorContext context){
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
         UserDto user = (UserDto) obj;
         return user.getPassword().equals(user.getConfirmPassword());
     }
